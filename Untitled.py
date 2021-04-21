3058a850e262fb1f91c36d6e4b03c14ce0dba27a
Nafisada <82958854+Nafisada@users.noreply.github.com>
2021-04-21 23:45:47 +0700

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Nama : Nafisa Dian Anfusana
+#NIM : 20/455420/PA/19635
+print ("Halo semua jangan lupa jaga kesehatan")
+
+
+# In[ ]:
+
+
+
+
